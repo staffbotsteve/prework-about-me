9190aed130d9690f1eb4f5862763ae4f79b63f1f
Steven Swan <sactoswan@gmail.com>
2020-04-17 07:55:02 -0700

    Adding changes

---
+++ module6/javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height="250px";
+});
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.color="blue";
+});
